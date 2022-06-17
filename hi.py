a376e041b71855d8846e2cc145c557f9b2c35895
Sanilya Panchal <sanilya@softvan.in>
2022-06-17 13:02:36 +0530

    11

--- hi.py
+++ hi.py
@@ -1,1 +1,14 @@
-print("hi")
+#!/usr/bin/env python3
+
+import boto3
+
+AWS_REGION = "us-east-2"
+
+resource = boto3.resource("s3", region_name=AWS_REGION)
+
+iterator = resource.buckets.all()
+
+print("Listing Amazon S3 Buckets:")
+
+for bucket in iterator:
+    print(f"-- {bucket.name}")
