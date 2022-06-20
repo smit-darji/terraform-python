80211a8748aca7ec923260bafaefb045d631f1ec
smit-darji <smit.softvan@gmail.com>
2022-06-20 12:16:07 +0530

    test

--- Terrafrom/defaultdb.tf
+++ Terrafrom/defaultdb.tf
@@ -1,5 +1,5 @@
 # 9.1.3
-resource "aws_lakeformation_permissions" "DbPermition17" {
+resource "aws_lakeformation_permissions" "DbDefault" {
   principal   = "arn:aws:iam::958500205200:role/sdlf-dataengg-mlspl-Curated-GlueJob"
   permissions = ["ALL"]
   database {
